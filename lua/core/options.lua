e2e2098cce9f9ad86554769ff9c535c913fa7879
David Surkov <surkovdavid@gmail.com>
2025-04-15 17:49:50 +0200

    added git status to telescope

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,6 @@
 -- core/options.lua: General settings
 
-vim.o.hlsearch = false          -- Don't highlight searches
+vim.o.hlsearch = true
 vim.wo.number = true            -- Show line numbers
 vim.wo.relativenumber = true    -- Show relativenumber on other lines
 vim.o.mouse = 'a'               -- Enable mouse mode
@@ -8,7 +8,7 @@ vim.o.clipboard = 'unnamedplus' -- Sync system clipboard
 vim.o.termguicolors = true      -- Enable 24-bit colors
 vim.o.breakindent = true        -- Enable break indent
 vim.o.undofile = true           -- Enable persistent undo
-vim.o.ignorecase = true         -- Case-insensitive searching
+--[[ vim.o.ignorecase = true         -- Case-insensitive searching ]]
 vim.o.smartcase = true          -- Case-sensitive if uppercase present
 vim.wo.signcolumn = 'yes'       -- Always show sign column
 vim.o.updatetime = 250          -- Faster completion
