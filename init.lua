98c98c6b6e37b95cc5bdfb908dd5457919736052
David Surkov <surkovdavid@gmail.com>
2025-05-25 22:37:35 +0200

    reworked structure and added couple new plugins. swapped telescope with fzf

--- init.lua
+++ init.lua
@@ -3,23 +3,10 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- Load plugins (which will use lazy.nvim)
-require("plugins")
 
--- Load core modules
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
+vim.g.have_nerd_font = true
 
--- Load Mason setup first if it's in its own module or directly in the lsp module.
--- For instance, if your lsp.lua starts with:
-require('mason').setup()
--- require('mason-lspconfig').setup({automatic_installation = true})
-
--- Load additional configuration for specific plugins
-require("config.telescope")
-require("config.treesitter")
-require("config.lsp")
-
--- Optionally load custom plugins (overrides or additional plugins)
-require("custom.plugins")
+require 'globals'
+require 'options'
+require 'keymaps'
+require 'lazy-init'
--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -1,9 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
