f91c87b381a607140c005a41cf980e3c3b07b869
David Surkov <surkovdavid@gmail.com>
2026-07-07 21:44:37 +0200

    added lsp rename on file move/change (snacks-rename && mini)

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -249,6 +249,14 @@ return {
         },
       }
 
+      -- LSP-integrated file renaming
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesActionRename',
+        callback = function(event)
+          require('snacks').rename.on_rename_file(event.data.from, event.data.to)
+        end,
+      })
+
       vim.api.nvim_create_autocmd('User', {
         pattern = 'MiniFilesBufferCreate',
         callback = function(args)
